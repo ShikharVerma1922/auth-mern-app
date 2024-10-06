f025e2993c410d49c43dfc1bd6b1b2e8fed0e292
Shikhar Verma <shikharverma@Shikhars-MacBook-Air.local>
2024-10-06 13:03:52 +0530

    Initial commit

---
+++ backend/Models/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongo_url = process.env.MONGO_CONN;
+
+mongoose
+  .connect(mongo_url)
+  .then(() => {
+    console.log("MongoDB connected...");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error", err);
+  });
---
+++ backend/index.js
@@ -0,0 +1,26 @@
+// shikharverma1922 2C7iuXlsDqdBdjbX
+
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import "./Models/db.js";
+import AuthRouter from "./Routes/AuthRouter.js";
+import ProductRouter from "./Routes/ProductRouter.js";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/auth", AuthRouter);
+app.use("/products", ProductRouter);
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
